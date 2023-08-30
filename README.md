7d4a436931e15ec5a3661c84e4f7cfe6efd1458c
rabha79 <rabhagharib81@gmail.com>
2023-08-31 01:25:09 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+<!-- task 4 -->
+# simple calculator
+---
+## It is used to add, subtract, multiply, or divide two numbers
+---
+	```
+function calculate(number1,number2,method){
+    if(method == "sum"){
+        var result = number1 + number2 
+        console.log(result)
+    }else if(method == "sub"){
+        var result = number1 - number2 
+        console.log(result)
+    }else if(method == "multi") {
+        var result = number1 * number2 
+        console.log(result)
+    }else if(method == "div"){
+        var result = number1 / number2 
+        console.log(result)
+    }else{
+        console.log("unknown method , try again ")
+    }
+}
+calculate(2,3,"sum")
+calculate(2,3,"sub")
+calculate(2,3,"multi")
+calculate(2,3,"div")
+calculate(2,3,"rrr")
+```
+### this ia an example about calculator
--- javascript.js
+++ javascript.js
@@ -28,3 +28,61 @@ function getShawarma(name,age){
 }
 getShawarma("Rabha",21)
 getShawarma("dina",17)
+// task4
+function calculate(number1,number2,method){
+    if(method == "sum"){
+        var result = number1 + number2 
+        console.log(result)
+    }else if(method == "sub"){
+        var result = number1 - number2 
+        console.log(result)
+    }else if(method == "multi") {
+        var result = number1 * number2 
+        console.log(result)
+    }else if(method == "div"){
+        var result = number1 / number2 
+        console.log(result)
+    }else{
+        console.log("unknown method , try again ")
+    }
+}
+calculate(2,3,"sum")
+calculate(2,3,"sub")
+calculate(2,3,"multi")
+calculate(2,3,"div")
+calculate(2,3,"rrr")
+// task 5
+switch(3){
+    case 1:
+        console.log("January");
+        break;
+    case 2:    
+       console.log("February");
+       break; 
+    case 3:    
+       console.log("March");
+       break; 
+    case 4:    
+       console.log("April");
+       break; 
+    default:
+        console.log("This month there is no booking");      
+}
+// Another method
+switch(3){
+    case 1:
+        console.log("January");
+    case 2:    
+       console.log("February");
+    case 3:    
+       console.log("March");
+    case 4:    
+       console.log("April");     
+}
+// task 6
+var age = 17;
+var res = (age >= 30) ? "You are allowed to enter" : "You are not allowed to enter";
+console.log(res);  
+// Another method
+var res = ( 21%11 == 10 ) ? "the result is 10" : "the result is 5";
+console.log(res);
